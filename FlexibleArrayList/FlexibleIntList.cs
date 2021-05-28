a4f89e733b3178a70dc9e8a244e6130b9043ea9c
GET Academy <terje@kolderup.net>
2021-05-28 10:26:09 +0200

    Initial commit

---
+++ FlexibleArrayList/FlexibleIntList.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace FlexibleArrayList
+{
+    class FlexibleIntList
+    {
+        int[] _numbers = new int[2];
+        int _count = 0;
+
+        public int AddNumber(int number)
+        {
+            if (_count >= _numbers.Length) IncreaseCapacity();
+            _numbers[_count] = number;
+            _count++;
+            return _count;
+        }
+
+
+        public void Show()
+        {
+            for (var index = 0; index < _count; index++)
+            {
+                var n = _numbers[index];
+                Console.Write(n + " ");
+            }
+
+            Console.WriteLine();
+        }
+
+        private void IncreaseCapacity()
+        {
+            var length = _numbers.Length;
+            Console.WriteLine($"Øker kapasitet fra {length} til {length * 2}.");
+            var newNumbers = new int[length * 2];
+            Array.Copy(_numbers, newNumbers, length);
+            _numbers = newNumbers;
+        }
+    }
+}
---
+++ FlexibleArrayList/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace FlexibleArrayList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new FlexibleIntList();
+            while (true)
+            {
+                var number = AskForInt("Tall: ");
+                numbers.AddNumber(number);
+                numbers.Show();
+            }
+        }
+
+        private static int AskForInt(string prompt)
+        {
+            Console.Write(prompt);
+            var numberTxt = Console.ReadLine();
+            var number = Convert.ToInt32(numberTxt);
+            return number;
+        }
+    }
+}
